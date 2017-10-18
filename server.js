db7595bcc1fb2a137168d92d28d757fbe29f04a8
Biyanta Shah <shah.bi@husky.neu.edu>
2017-10-17 23:21:41 -0400

    Server changes to deploy to heroku

--- server.js
+++ server.js
@@ -13,13 +13,20 @@ var mongoose = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+if(process.env.MLAB_USER_BIYANTA) {
+    //connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    //    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    //    process.env.OPENSHIFT_APP_NAME;
+
+    var username = process.env.MLAB_USER_BIYANTA; // get from environment
+    var password = process.env.MLAB_PASSWORD_BIYANTA;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds141534.mlab.com:41534/heroku_rq8bm6xt';
 }
+
+
 var db = mongoose.connect(connectionString);
 
 var app = express();
@@ -47,10 +54,15 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//var ipaddress = '127.0.0.1';
+var port = process.env.PORT || 3000;
+
+//if (process.env.port) {
+//    ipaddress = 'www.biyanta-shah.herokuapp.com'
+//}
 
 require("./public/assignment/server/app.js")(app, db, mongoose);
 require("./public/project/server/app.js")(app,db,mongoose);
 
-app.listen(port, ipaddress);
+//app.listen(port, ipaddress);
+app.listen(port);
